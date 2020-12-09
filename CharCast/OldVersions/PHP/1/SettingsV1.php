16cbbf3aa2407ab7ce920399e45186e76eb1f949
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-08 20:00:45 -0800

    SettingsV1.php
    An archive of the first version of the settings file for SNU CharCast.

---
+++ CharCast/OldVersions/PHP/1/SettingsV1.php
@@ -0,0 +1,20 @@
+<?php
+// Settings for SNU Charcast (originally named SNU Beaucast)
+/* OBSOLETE 
+dropdown { .. Placeholders
+  $actor-animals = "";
+  $actor-digitals = "";
+  $actor-real.people = "";
+  $actor-adults = "";
+  $actor-children = "";
+  $actor-infants = "";
+}
+*/
+actorDropdown = array("Animal actors", "Digital actors", "Real actors", "Fictional actors", "Adult actors", "Child actors", "Infant actors");
+mediaTypeDropdown = array("Video", "Movie", "Book", "NSFW", "Image", "Photograph", "Book", "Article", "Blog");
+/* File info
+* File version: 1 (Tuesday, December 8th 2020 at 8:00 pm)
+* File type: PHP script file (*.php)
+* Line count (including blank lines and compiler line): 21
+*/
+?>
